7201de0b3899d49054fd8ba749bcf07343dd83ab
ChugunovRoman <Zebs-BMK@yandex.ru>
2017-09-04 14:35:52 +0300

    add setProps in Link.spec

--- test/components/Link.spec.js
+++ test/components/Link.spec.js
@@ -44,8 +44,14 @@ describe('Components: <Link />', () => {
         expect(wrapper).toMatchSnapshot();
 
         wrapper.find('a').simulate('click', { preventDefault });
+        // не уверен, что здесть правильно так тестировать
+        // изменение стейта после экшена.
+        wrapper.setProps({ active: true });
+        expect(wrapper.find('.link').hasClass('link_active')).toEqual(true);
         expect(props.onClick).toBeCalled();
         expect(preventDefault).toBeCalled();
+
+        expect(wrapper).toMatchSnapshot();
     });
 
 });
